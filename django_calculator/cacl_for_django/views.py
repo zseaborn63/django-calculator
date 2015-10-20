13e9ccc1f743385affc95966d1be922e012aaaf1
zseaborn63 <zseaborn63@gmail.com>
2015-10-20 13:09:37 -0400

    Calculator working!

--- django_calculator/cacl_for_django/views.py
+++ django_calculator/cacl_for_django/views.py
@@ -5,4 +5,24 @@ from django.shortcuts import render, render_to_response
 
 
 def math_view(request):
-    return render_to_response(template_name="math.html")
+    first_num = int(request.GET.get("first_num"))
+    second_num = int(request.GET.get("second_num"))
+    operator = request.GET.get("operator")
+    if operator == "Divide":
+        if second_num == 0:
+            calc = "You clearly don't understand math"
+        else:
+            calc = first_num / second_num
+    elif operator == "Multiply":
+        calc = first_num * second_num
+    elif operator == "Subtract":
+        calc = first_num - second_num
+    else:
+        calc = first_num + second_num
+    context =  {
+        "calculation": "{}".format(calc),
+        "first_number": first_num,
+        "second_number": second_num,
+        "operator": operator
+                }
+    return render_to_response(template_name="math.html", context=context)
